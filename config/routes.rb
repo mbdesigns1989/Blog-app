d8b7aaf23440d642a4ce65a235fc7914657db1b3
mbdesigns1989 <mbdesigns1989@yahoo.com>
2022-08-16 22:15:27 +0300

    Fix linters errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'users#index'
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Defines the root path route ("/")
+  root "users#index"
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
   end
 end
-

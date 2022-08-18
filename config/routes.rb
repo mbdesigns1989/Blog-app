446ac5d850dfb13944fb9c8148619d5fc8741354
mbdesigns1989 <mbdesigns1989@yahoo.com>
2022-08-18 03:52:33 +0300

    Add new post routes and views

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "users#index"
+  resources :users, only: [:index, :show]  do
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create, :destroy]
+    end
+  end
+
+  resources :posts, only: [:new, :create]
+end

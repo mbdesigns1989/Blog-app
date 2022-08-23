cd4b9de1d8a1c8603b0749feb494fee4664cc0ae
David Abol Ger <abolger254@gmail.com>
2022-08-23 11:00:16 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id'
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
-
+  
+  @post = current_user.posts.new(new_post_params)
+  
   def three_recent_posts
     Post.last(3)
   end

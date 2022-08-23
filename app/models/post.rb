7d485329180b4c7672d1e10a7faf13cbe2b08ff9
Tufahel Ahmed <tufahel96@gmail.com>
2022-08-23 14:02:19 +0600

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  redirect_to user_post_path(current_user.id, @post.id) 
 
   belongs_to :author, class_name: 'User'
   has_many :comments
